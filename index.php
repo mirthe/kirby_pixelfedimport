c180b095cf4a41141157d962927f207a954f4303
Mirthe Valentijn <me@mirthe.org>
2026-05-28 17:16:13 +0200

    Fix admin route checks

--- index.php
+++ index.php
@@ -1,45 +1,50 @@
 <?php
-$kirby = kirby();
-if (($user = $kirby->user()) && $user->role()->id() === 'admin') {
+Kirby::plugin('mirthe/pixelfed-import', [
+    'options' => [
+        'token' => option('pixelfed.token'),
+        'userid' => option('pixelfed.userid'),
+        'limit' => option('pixelfed.limit'),
+        'since_id' => option('pixelfed.since_id'),
+        'contentsubfolder' => option('pixelfed.contentsubfolder')
+    ],
 
-    Kirby::plugin('mirthe/pixelfed-import', [
-        'options' => [
-            'token' => option('pixelfed.token'),
-            'userid' => option('pixelfed.userid'),
-            'limit' => option('pixelfed.limit'),
-            'since_id' => option('pixelfed.since_id'),
-            'contentsubfolder' => option('pixelfed.contentsubfolder')
-        ],
-
-        'routes' => [
-            [
-                'pattern' => 'pixelfed/getaccount',
-                'action'  => function () {
+    'routes' => [
+        [
+            'pattern' => 'pixelfed/getaccount',
+            'action'  => function () {
+                $kirby = kirby();
+                if (($user = $kirby->user()) && $user->role()->id() === 'admin') {
                     include 'getaccount.php';
                 }
-            ],
-            [
-                'pattern' => 'pixelfed/getlatest',
-                'action'  => function () {
+            }
+        ],
+        [
+            'pattern' => 'pixelfed/getlatest',
+            'action'  => function () {
+                $kirby = kirby();
+                if (($user = $kirby->user()) && $user->role()->id() === 'admin') {
                     $exportdir = __DIR__ . '/temp/'; // TODO move to config, use my fotofeed folder
                     include 'getlatest.php';
                 }
-            ],
-            [
-                'pattern' => 'pixelfed/pickone',
-                'action'  => function () {
+            }
+        ],
+        [
+            'pattern' => 'pixelfed/pickone',
+            'action'  => function () {
+                $kirby = kirby();
+                if (($user = $kirby->user()) && $user->role()->id() === 'admin') {
                     include 'pickone.php';
                 }
-            ],
-            [
-                'pattern' => 'pixelfed/getone',
-                'action'  => function () {
+            }
+        ],
+        [
+            'pattern' => 'pixelfed/getone',
+            'action'  => function () {
+                $kirby = kirby();
+                if (($user = $kirby->user()) && $user->role()->id() === 'admin') {
                     include 'getone.php';
                 }
-            ]
-            
+            }
         ]
-        
-    ]);
-
-;}
+    ]
+]);
